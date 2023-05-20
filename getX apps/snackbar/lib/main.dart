cbeb1b3924e57617f899934d2dc6f6cbb6d51cd5
M-Farjad <2021SE56@student.uet.edu.pk>
2023-05-20 17:10:16 +0500

    getx snackbar

--- getX apps/snackbar/lib/main.dart
+++ getX apps/snackbar/lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +13,59 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return GetMaterialApp(
+      title: 'SnackBar',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Container(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('SnackBar'),
+        ),
+        body: Column(
+          children: [
+            TextButton(
+              onPressed: () {
+                Get.snackbar(
+                  'Snack Bar title', 'Snack Bar message',
+                  snackPosition: SnackPosition.BOTTOM,
+                  animationDuration: Duration(milliseconds: 3000),
+
+                  /// `borderWidth: 4, borderColor: Colors.purple` is setting the width and color of the
+                  /// border around the snackbar. In this case, it is setting the border width to 4 and
+                  /// They must be used in combined
+                  // borderWidth: 4,
+                  // borderColor: Colors.deepPurple,
+
+                  backgroundGradient: const LinearGradient(
+                    colors: [Colors.red, Colors.deepPurple],
+                  ),
+                  dismissDirection: DismissDirection.horizontal,
+                  isDismissible: true,
+                  forwardAnimationCurve: Curves.bounceInOut,
+                  icon: const Icon(Icons.close),
+                  // shouldIconPulse: false,
+                  // leftBarIndicatorColor: Colors.white,
+                  mainButton: TextButton(
+                    onPressed: () {},
+                    child: const Text('Hello'),
+                  ),
+
+                  /// `onTap` is a callback function that is triggered when the user taps on the
+                  /// snackbar. In this case, it logs a message to the console using the `log`
+                  /// function from the `dart:developer` library.
+                  onTap: (val) {
+                    log('SnackBar message');
+                  },
+                  showProgressIndicator: true,
+                );
+              },
+              child: const Text('CLink me'),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
