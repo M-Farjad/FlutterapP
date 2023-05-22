37c94b670408449bb8c0953bfffdc56161b5418c
M-Farjad <2021SE56@student.uet.edu.pk>
2023-05-22 23:22:34 +0500

    showDialog getx

--- getX apps/basic_getxops/lib/APIs/apis.dart
+++ getX apps/basic_getxops/lib/APIs/apis.dart
@@ -39,4 +39,33 @@ class APIs {
       showProgressIndicator: true,
     );
   }
+
+  static Future<dynamic> showDialog() {
+    return Get.defaultDialog(
+      title: 'Button',
+      //? titleStyle: to style the title
+
+      /// `middleText` is setting the text that will be displayed in the middle of the
+      /// dialog box that will appear when the button is clicked. `middleTextStyle` is
+      /// used to set the style of the text in the middle of the dialog box.
+      middleText: 'Hello from Button',
+      //? middleTextStyle: to style the title
+      content: const Row(
+        children: [
+          CircularProgressIndicator(),
+          SizedBox(width: 10),
+          Expanded(child: Text('Hello from Button')),
+        ],
+      ),
+      textCancel: 'Cancel',
+      textConfirm: 'Confirm',
+      onCancel: () {},
+      onConfirm: () {},
+
+      // actions: [] to add mutiple widgets
+      //? to customize the buttons
+      //? cancel: Widget, confirm: Widget,
+      // barrierDismissible: false //!allow barrier to dismiss
+    );
+  }
 }
