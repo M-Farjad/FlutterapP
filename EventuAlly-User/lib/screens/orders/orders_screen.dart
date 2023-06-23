4d66bf7661ed2b67ea850c52c576ca2fb711df6d
M-Farjad <2021SE56@student.uet.edu.pk>
2023-06-23 18:57:20 +0500

    EventuAlly OrdersInitial

--- EventuAlly-User/lib/screens/orders/orders_screen.dart
+++ EventuAlly-User/lib/screens/orders/orders_screen.dart
@@ -11,8 +11,83 @@ class OrdersScreen extends StatelessWidget {
         leading: const Icon(CupertinoIcons.back),
         centerTitle: true,
         elevation: 0,
-        title: const Text('Your Orders'),
+        title: const Text(
+          'Your Orders',
+          style: TextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 20,
+          ),
+        ),
       ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 142,
+                height: 37,
+                decoration: ShapeDecoration(
+                  color: Color(0xFF34A853),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18),
+                  ),
+                  shadows: [
+                    BoxShadow(
+                      color: Color(0x3F4285F4),
+                      blurRadius: 12,
+                      offset: Offset(0, 4),
+                      spreadRadius: 0,
+                    )
+                  ],
+                ),
+                child: Center(
+                  child: Text(
+                    'Active orders',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14.18,
+                      fontFamily: 'Manrope',
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: 139.42,
+                height: 37.81,
+                decoration: ShapeDecoration(
+                  color: Color(0xFFCB585A),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18),
+                  ),
+                  shadows: [
+                    BoxShadow(
+                      color: Color(0x3F4285F4),
+                      blurRadius: 12,
+                      offset: Offset(0, 4),
+                      spreadRadius: 0,
+                    )
+                  ],
+                ),
+                child: Center(
+                  child: Text(
+                    'All orders',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14.18,
+                      fontFamily: 'Manrope',
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ), // TODO add orders list here
     );
   }
 }
