b48bbcbf938c595fc72439ed8bacf8376a0b30aa
M-Farjad <2021SE56@student.uet.edu.pk>
2023-06-28 18:47:27 +0500

    EventuAlly OrderStatusIntitial5

--- EventuAlly-User/lib/screens/order_status/order_status_screen.dart
+++ EventuAlly-User/lib/screens/order_status/order_status_screen.dart
@@ -21,185 +21,198 @@ class OrderStatusScreen extends StatelessWidget {
           child: Padding(
         padding: EdgeInsets.symmetric(horizontal: mq.width * .08),
         child: Center(
-          child: Column(
-            children: [
-              Text('Order Status', style: kBlackTextStyle),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                    horizontal: mq.width * .02, vertical: mq.height * .02),
-                child: StepProgressIndicator(
-                  totalSteps: 3,
-                  currentStep: 1,
-                  size: mq.height * .08,
-                  padding: mq.width * .006,
-                  selectedColor: Color(constant.red),
-                  unselectedColor: Colors.grey,
-                  customStep: makeProgressSteps,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text('Order Status', style: kBlackTextStyle),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: mq.width * .02, vertical: mq.height * .02),
+                  child: StepProgressIndicator(
+                    totalSteps: 3,
+                    currentStep: 1,
+                    size: mq.height * .08,
+                    padding: mq.width * .006,
+                    selectedColor: Color(constant.red),
+                    unselectedColor: Colors.grey,
+                    customStep: makeProgressSteps,
+                  ),
                 ),
-              ),
-              Container(
-                width: double.infinity,
-                height: mq.height * .3,
-                decoration: ShapeDecoration(
-                  shape: RoundedRectangleBorder(
+                Container(
+                  width: double.infinity,
+                  height: mq.height * .3,
+                  decoration: ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  ),
+                  child: ClipRRect(
                     borderRadius: BorderRadius.circular(16),
+                    child: Image.asset(
+                      'assets/images/caterers.jpg',
+                      fit: BoxFit.fill,
+                    ),
                   ),
                 ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(16),
-                  child: Image.asset(
-                    'assets/images/caterers.jpg',
-                    fit: BoxFit.fill,
+                Text(
+                  'Shadi Package',
+                  style: TextStyle(
+                    color: Color(constant.black),
+                    fontSize: 24,
+                    fontFamily: 'Manrope',
+                    fontWeight: FontWeight.w800,
                   ),
                 ),
-              ),
-              Text(
-                'Shadi Package',
-                style: TextStyle(
-                  color: Color(constant.black),
-                  fontSize: 24,
-                  fontFamily: 'Manrope',
-                  fontWeight: FontWeight.w800,
+                Text('Order Number: 098765', style: kRedTextStyle),
+                SizedBox(height: mq.height * .015),
+                const Text(
+                  'Biryani, Qorma, Kheer, Kabab, Gulab Jamun, Pulao, Chapati, Naan, Broast & Custard. Biryani, Qorma, Kheer, Kabab, Gulab Jamun, Pulao, Chapati, Naan, Broast & Custard.',
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                    color: Color(0xFF555454),
+                    fontSize: 12,
+                    fontFamily: 'Manrope',
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              ),
-              Text('Order Number: 098765', style: kRedTextStyle),
-              const Text(
-                'Biryani, Qorma, Kheer, Kabab, Gulab Jamun, Pulao, Chapati, Naan, Broast & Custard. Biryani, Qorma, Kheer, Kabab, Gulab Jamun, Pulao, Chapati, Naan, Broast & Custard.',
-                textAlign: TextAlign.justify,
-                style: TextStyle(
-                  color: Color(0xFF555454),
-                  fontSize: 12,
-                  fontFamily: 'Manrope',
-                  fontWeight: FontWeight.w500,
+                SizedBox(height: mq.height * .015),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Price',
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(
+                        color: Color(0x7F555454),
+                        fontSize: 12,
+                        fontFamily: 'Manrope',
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    Text(
+                      'Person',
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        color: Color(0x7F555454),
+                        fontSize: 12,
+                        fontFamily: 'Manrope',
+                        fontWeight: FontWeight.w700,
+                      ),
+                    )
+                  ],
                 ),
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Price',
-                    textAlign: TextAlign.justify,
-                    style: TextStyle(
-                      color: Color(0x7F555454),
-                      fontSize: 12,
-                      fontFamily: 'Manrope',
-                      fontWeight: FontWeight.w700,
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '50,000 Rs',
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(
+                        color: Color(0xFF555454),
+                        fontSize: 24,
+                        fontFamily: 'Manrope',
+                        fontWeight: FontWeight.w700,
+                      ),
                     ),
-                  ),
-                  Text(
-                    'Person',
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                      color: Color(0x7F555454),
-                      fontSize: 12,
-                      fontFamily: 'Manrope',
-                      fontWeight: FontWeight.w700,
+                    Text(
+                      '100',
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        color: Color(0xFF555454),
+                        fontSize: 24,
+                        fontFamily: 'Manrope',
+                        fontWeight: FontWeight.w700,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: mq.height * .015),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: mq.width * .16,
+                      child: const Text(
+                        'Date',
+                        style: TextStyle(
+                          color: Color(0xFF555454),
+                          fontSize: 14,
+                          fontFamily: 'Manrope',
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
                     ),
-                  )
-                ],
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    '50,000 Rs',
-                    textAlign: TextAlign.justify,
-                    style: TextStyle(
-                      color: Color(0xFF555454),
-                      fontSize: 24,
-                      fontFamily: 'Manrope',
-                      fontWeight: FontWeight.w700,
+                    const Expanded(
+                      child: Text(
+                        '21/05/2023',
+                        style: TextStyle(
+                          color: Color(0xFFCB585A),
+                          fontSize: 16,
+                          fontFamily: 'Manrope',
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: mq.width * .16,
+                      child: const Text(
+                        'Location',
+                        style: TextStyle(
+                          color: Color(0xFF555454),
+                          fontSize: 14,
+                          fontFamily: 'Manrope',
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
                     ),
-                  ),
-                  Text(
-                    '100',
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                      color: Color(0xFF555454),
-                      fontSize: 24,
-                      fontFamily: 'Manrope',
-                      fontWeight: FontWeight.w700,
+                    const SizedBox(
+                      width: 258,
+                      child: Text(
+                        'XYZ building XZ block, Latifabad, Hyd. ',
+                        style: TextStyle(
+                          color: Color(0xFFCB585A),
+                          fontSize: 16,
+                          fontFamily: 'Manrope',
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: mq.width * .16,
+                      child: const Text(
+                        'Duration',
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                          color: Color(0xFF555454),
+                          fontSize: 14,
+                          fontFamily: 'Manrope',
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
                     ),
-                  )
-                ],
-              ),
-              const Row(
-                children: [
-                  Text(
-                    'Date',
-                    textAlign: TextAlign.justify,
-                    style: TextStyle(
-                      color: Color(0xFF555454),
-                      fontSize: 14,
-                      fontFamily: 'Manrope',
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  Expanded(
-                    child: Text(
-                      '21/05/2023',
-                      textAlign: TextAlign.center,
+                    const Text(
+                      '6:00 pm to 12:00 am',
+                      textAlign: TextAlign.justify,
                       style: TextStyle(
                         color: Color(0xFFCB585A),
                         fontSize: 16,
                         fontFamily: 'Manrope',
                         fontWeight: FontWeight.w700,
                       ),
-                    ),
-                  )
-                ],
-              ),
-              const Row(
-                children: [
-                  Text(
-                    'Location',
-                    textAlign: TextAlign.justify,
-                    style: TextStyle(
-                      color: Color(0xFF555454),
-                      fontSize: 14,
-                      fontFamily: 'Manrope',
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 258,
-                    child: Text(
-                      'XYZ building XZ block, Latifabad, Hyd. ',
-                      style: TextStyle(
-                        color: Color(0xFFCB585A),
-                        fontSize: 16,
-                        fontFamily: 'Manrope',
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                  )
-                ],
-              ),
-              const Row(
-                children: [
-                  Text(
-                    'Duration',
-                    textAlign: TextAlign.justify,
-                    style: TextStyle(
-                      color: Color(0xFF555454),
-                      fontSize: 14,
-                      fontFamily: 'Manrope',
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  Text(
-                    '6:00 pm to 12:00 am',
-                    textAlign: TextAlign.justify,
-                    style: TextStyle(
-                      color: Color(0xFFCB585A),
-                      fontSize: 16,
-                      fontFamily: 'Manrope',
-                      fontWeight: FontWeight.w700,
-                    ),
-                  )
-                ],
-              )
-            ],
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       )),
