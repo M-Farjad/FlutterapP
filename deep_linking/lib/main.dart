b08ef1f05af42e49d0d272d353f6b3d3627dbc3f
M-Farjad <2021SE56@student.uet.edu.pk>
2023-10-09 23:05:51 +0500

    DeepLinkingInit

---
+++ deep_linking/lib/main.dart
@@ -0,0 +1,130 @@
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp.router(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       initialRoute: '/',
+//       getPages: [
+//         GetPage(
+//           name: '/',
+//           page: () => const MyHomePage(title: 'Flutter Demo Home Page'),
+//           transition: Transition.zoom,
+//         ),
+//         GetPage(
+//           name: '/profile',
+//           page: () => Scaffold(appBar: AppBar(title: const Text('Profile'))),
+//           transition: Transition.zoom,
+//         ),
+//       ],
+//       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+//     );
+
+//     // return GetMaterialApp(
+//     //   title: 'Flutter Demo',
+//     //   theme: ThemeData(
+//     //     colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//     //     useMaterial3: true,
+//     //   ),
+//     //   initialRoute: '/',
+//     //   getPages: [
+//     //     GetPage(
+//     //       name: '/',
+//     //       page: () => const MyHomePage(title: 'Flutter Demo Home Page'),
+//     //       transition: Transition.zoom,
+//     //     ),
+//     //     GetPage(
+//     //       name: '/profile',
+//     //       page: () => Scaffold(appBar: AppBar(title: const Text('Profile'))),
+//     //       transition: Transition.zoom,
+//     //     ),
+//     //   ],
+//     //   // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+//     // );
+
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+//   final String title;
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headlineMedium,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+void main() => runApp(MaterialApp.router(routerConfig: router));
+
+final router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (_, __) => Scaffold(
+        appBar: AppBar(title: const Text('Home Screen')),
+      ),
+      routes: [
+        GoRoute(
+          path: 'details',
+          builder: (_, __) => Scaffold(
+            appBar: AppBar(title: const Text('Details Screen')),
+          ),
+        ),
+      ],
+    ),
+  ],
+);
